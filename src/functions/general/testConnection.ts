d02b8b533917d0ecf6e81c1e85cc47da19db3017
OnCloud <anonymousaaaa41414141@gmail.com>
2024-12-21 07:10:49 +0800

    refactor: improve typing of `general/testConnection` function

--- src/functions/general/testConnection.ts
+++ src/functions/general/testConnection.ts
@@ -1,8 +1,22 @@
 import { HttpRequestMethods, sendRequest } from "../sendRequest";
 
-export async function testConnection() {
-  return await sendRequest<[]>(HttpRequestMethods.GET, ["connect"], {
-    passToken: true,
-    timeout: 3 * 1000,
-  });
+export async function testConnection(
+  testConnectionOptions?: TestConnectionOptions,
+) {
+  const { timeout } = testConnectionOptions || {};
+
+  return await sendRequest<TestConnectionResponse>(
+    HttpRequestMethods.GET,
+    ["connect"],
+    {
+      passToken: true,
+      timeout: timeout || 3 * 1000,
+    },
+  );
 }
+
+export type TestConnectionOptions = {
+  timeout?: number;
+};
+
+export type TestConnectionResponse = [];
